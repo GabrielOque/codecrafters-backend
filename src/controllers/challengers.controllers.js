7923ff5b81df1c582ed8738527b8e98635977ceb
Gabriel Oquendo <oquendogabriel18@gmail.com>
2024-04-07 18:39:34 -0500

    add challengers controllers

---
+++ src/controllers/challengers.controllers.js
@@ -0,0 +1,22 @@
+import Challenger from "../models/challengers.models.js";
+
+export const getChallengers = async (req, res) => {
+    try {
+        const challengers = await Challenger.find();
+        return res.send(challengers);
+    } catch (error) {
+        console.log(error);
+        res.send({ message: "Ocurrio un mensaje" });
+    }
+};
+
+export const createChallenger = async (req, res) => {
+    try {
+        const challenger = new Challenger(req.body);
+        const challengerCreated = await challenger.save();
+        return res.send(challengerCreated);
+    } catch (error) {
+        console.log(error);
+        res.send({ message: "Ocurrio un mensaje" });
+    }
+};
